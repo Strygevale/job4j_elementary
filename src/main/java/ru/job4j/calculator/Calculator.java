071e6eea7e69104dc0080a45ce456b65f109ee80
“Андрей <andrey272242@mail.ru>
2021-12-19 16:19:40 +0600

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,10 +1,13 @@
 package ru.job4j.calculator;
 
 public class Calculator {
+
     public static void main(String[] args) {
-        int size = 10;
-        size = 100;
-        size = size - 5;
-        System.out.println(size);
+        Calculator.plus(1, 3);
+    }
+
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
     }
 }
