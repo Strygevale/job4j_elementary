dea93ab6e9251766dece1091a92be4b3805ab564
“Андрей <andrey272242@mail.ru>
2021-12-20 19:05:03 +0600

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,19 +2,25 @@ package ru.job4j.converter;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-        float rsl = value / 83;
+        float rsl = value / 80;
         return rsl;
     }
 
     public static float rubleToDollar(float value) {
-            float rsl = value / 74;
-            return rsl;
+        float rsl = value / 70;
+        return rsl;
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar(200);
-        System.out.println("140 rubles are " + euro + " euro");
-        System.out.println("200 rubles are " + dollar + " dollar");
+        float rubEu = 160;
+        float rubDollar = 210;
+        float euro = 2;
+        float dollar = 3;
+        float outEu = Converter.rubleToEuro(rubEu);
+        float outDollar = Converter.rubleToDollar(rubDollar);
+        boolean passedEu = euro == outEu;
+        boolean passedDollar = dollar == outDollar;
+        System.out.println("160 rubles are 2 euro. Test result : " + passedEu);
+        System.out.println("210 rubles are 3 dollar. Test result : " + passedDollar);
     }
 }
