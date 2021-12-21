8ed648b3a579aa1cda595a9680b00f11522ac04e
“Андрей <andrey272242@mail.ru>
2021-12-21 18:10:08 +0600

    1.1. Что такое import [#504773 #242195]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -1,8 +1,9 @@
-package ru.job4j.math;
+package ru.job4j.calculator;
+
 import static ru.job4j.math.MathFunction.*;
+import static ru.job4j.math.MathFunction.division;
 
 public class MathCalculator {
-
     public static double sumAndMultiply(double first, double second) {
         return sum(first, second)
                 + multiply(first, second);
@@ -23,6 +24,6 @@ public class MathCalculator {
     public static void main(String[] args) {
         System.out.println("sumAndMultiply: " + sumAndMultiply(10, 20));
         System.out.println("subAndDivision: " + subAndDivision(40, 10));
-        System.out.println("sumAll: " + sumAndMultiply(15, 20));
+        System.out.println("sumAll: " + sumAll(15, 20));
     }
 }
