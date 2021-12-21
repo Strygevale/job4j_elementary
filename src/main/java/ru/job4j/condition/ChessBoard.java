506c3728e807d1ce62d922b10cbae05e1c6d24d5
“Андрей <andrey272242@mail.ru>
2021-12-21 19:52:34 +0600

    4.3.6. Math.abs [#373616]

---
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+
+    public static int way(int x1, int y1, int x2, int y2) {
+        int rsl = 0;
+        boolean board = boardFields(x1) && boardFields(y1) && boardFields(x2) && boardFields(y2);
+        if (board && (Math.abs(x2 - x1) == Math.abs(y2 - y1))) {
+            rsl = Math.abs(x2 - x1);
+        }
+        return rsl;
+    }
+
+    public static boolean boardFields(int num) {
+        return num >= 0 || num >= 7;
+    }
+
+}
