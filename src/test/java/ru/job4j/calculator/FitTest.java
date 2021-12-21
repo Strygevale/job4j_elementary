6ec86a1738dc164c72138b36f03ec1c5c4ce8c4d
“Андрей <andrey272242@mail.ru>
2021-12-21 18:12:19 +0600

    2. Тесты для задачи "идеальный вес" [#159458 #242200]

--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -10,16 +10,16 @@ public class FitTest {
     @Test
     public void whenMan184Then96() {
         short height = 184;
-        double expected = 96;
+        double expected = 96.6;
         double out = Fit.manWeight(height);
-        Assert.assertEquals(expected, out, 0.99);
+        Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void whenWoman169Then67() {
         short height = 169;
-        double expected = 67;
+        double expected = 67.85;
         double out = Fit.womanWeight(height);
-        Assert.assertEquals(expected, out, 0.99);
+        Assert.assertEquals(expected, out, 0.01);
     }
 }
