7e051ff97d3816f658601d68109e6a2bcfa97e48
“Андрей <andrey272242@mail.ru>
2021-12-22 20:32:52 +0600

    5.2. Создать программу, вычисляющую факториал. [#193]

---
+++ src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FactorialTest {
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundredTwenty() {
+        int n = 5;
+        int res = Factorial.calc(n);
+        int expected = 120;
+        Assert.assertEquals(expected, res);
+    }
+
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        int n = 0;
+        int res = Factorial.calc(n);
+        int expected = 1;
+        Assert.assertEquals(expected, res);
+    }
+}
