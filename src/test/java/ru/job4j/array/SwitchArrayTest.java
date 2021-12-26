0e77422a917fbb8783126ce54e4a3a1aa8508c74
“Андрей <andrey272242@mail.ru>
2021-12-26 10:54:30 +0600

    6.2.2. Переставить элементы массива [#226795 #243831]

--- src/test/java/ru/job4j/array/SwitchArrayTest.java
+++ src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -16,4 +16,25 @@ public class SwitchArrayTest {
         int[] expected = {4, 2, 3, 1};
         Assert.assertArrayEquals(expected, result);
     }
+
+    @Test
+    public void whenNoSwap() {
+        int[] input = new int[] {2, 3, 0, 5};
+        int source = 1;
+        int dest = 2;
+        int[] result = SwitchArray.swap(input, source, dest);
+        int[] expected = new int[] {2, 0, 3, 5};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSwap3To6() {
+        int[] input = new int[] {2, 1, 4, 6, 8, 1};
+        int source = 2;
+        int dest = 3;
+        int[] result = SwitchArray.swap(input, source, dest);
+        int[] expected = new int[] {2, 1, 6, 4, 8, 1};
+        Assert.assertArrayEquals(expected, result);
+    }
+
 }
