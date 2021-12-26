316ccfa67a5ca6c09bb5c90fffec57b8551eeb90
“Андрей <andrey272242@mail.ru>
2021-12-26 16:02:20 +0600

    6.5.3. Сортировка выборкой. [#156320]

--- src/test/java/ru/job4j/array/SortSelectedTest.java
+++ src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -6,12 +6,27 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SortSelectedTest {
-
     @Test
     public void whenSort() {
         int[] data = new int[] {3, 4, 1, 2, 5};
         int[] result = SortSelected.sort(data);
         int[] expected = new int[] {1, 2, 3, 4, 5};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSort2() {
+        int[] data = new int[] {3, 4, 1};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 3, 4};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSort3() {
+        int[] data = new int[] {12, 3, 1, 7, 6, 2};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 3, 6, 7, 12};
         Assert.assertArrayEquals(expected, result);
     }
 }
