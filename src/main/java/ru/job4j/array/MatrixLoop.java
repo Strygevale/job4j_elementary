a3592986994a0a44d18510dc6df0e507bb9b77b6
“Андрей <andrey272242@mail.ru>
2021-12-26 19:02:42 +0600

    6.6.2. Двухмерный массив. Циклы. [#306725]

--- src/main/java/ru/job4j/array/MatrixLoop.java
+++ src/main/java/ru/job4j/array/MatrixLoop.java
@@ -2,6 +2,7 @@ package ru.job4j.array;
 
 public class MatrixLoop {
     public static void main(String[] args) {
+
         int[][] arr = {{4, 2, 3}, {7, 1}, {3, 5, 6, 4}};
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
--- src/main/java/ru/job4j/array/MatrixSum.java
+++ src/main/java/ru/job4j/array/MatrixSum.java
@@ -2,6 +2,7 @@ package ru.job4j.array;
 
 public class MatrixSum {
     public static int sum(int[][] array) {
+
         int rsl = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
