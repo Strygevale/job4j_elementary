c3744b2e3d48693839ad1c2a04064dcf691e278e
“Андрей <andrey272242@mail.ru>
2021-12-26 15:09:10 +0600

    6.4.2. Слово заканчивается на ... [#156317 #244201]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -2,6 +2,12 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        return word[word.length - 1] == post[post.length - 1];
+
+        for (int i = 0; i < post.length; i++) {
+           if (word[word.length - 1 - i] != post[post.length - 1 - i]) {
+               return false;
+           }
+        }
+        return true;
     }
 }
