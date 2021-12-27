32ea5b63182f4c4318d9a2262f1cbf8a48ad5a0c
“Андрей <andrey272242@mail.ru>
2021-12-27 19:58:37 +0600

    6.8. Дефрагментация массива [#225 #244609]

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -5,8 +5,7 @@ public class Defragment {
     public static String[] compress(String[] array) {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
-                int point = index;
-                for (int j = point + 1; j < array.length; j++) {
+                for (int j = index + 1; j < array.length; j++) {
                     if (array[j] != null) {
                         SwitchArray.swap(array, index, j);
                         break;
